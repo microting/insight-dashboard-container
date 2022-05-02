59c88b2156ac4a5ea1026025b8e63d736a1bed2f
René Schultz Madsen <rm@microting.com>
2022-05-02 06:05:11 +0000

    Bumping due to updates in deployment and frontend.

--- upgrade-all.sh
+++ upgrade-all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-CUSTOMERS=('128' '309' '36' '742' '779' '789')
+CUSTOMERS=('128' '309' '36' '740' '742' '779' '789')
 
 for CUSTOMER in ${CUSTOMERS[@]}; do
   echo "Updating customer with no $CUSTOMER"
   ./upgrade.sh $CUSTOMER
   echo "Done updating customer with no $CUSTOMER, sleeping 60 seconds before proceeding to the next"
-  sleep 90
+  sleep 30
 done
